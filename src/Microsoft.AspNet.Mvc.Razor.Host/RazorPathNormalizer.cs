e56284c2a829e6aff70fa72b65bb8a731cbf8e97
N. Taylor Mullen <nimullen@microsoft.com>
2015-03-20 14:26:21 -0700

    WIP

---
+++ src/Microsoft.AspNet.Mvc.Razor.Host/RazorPathNormalizer.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNet.Mvc.Razor.Internal
+{
+    public class RazorPathNormalizer
+    {
+        public virtual string NormalizePath(string path)
+        {
+            return path;
+        }
+    }
+}
